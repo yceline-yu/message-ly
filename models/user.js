15a7abdf0692a90a5e4300a1f341182b564e71bc
yceline-yu <cellystuff5@gmail.com>
2021-05-17 16:31:10 -0700

    user model finish

--- models/user.js
+++ models/user.js
@@ -5,9 +5,6 @@ const NotFoundError = require("../expressError")
 const { BCRYPT_WORK_FACTOR } = require('../config');
 
 
-
-
-
 /** User of the site. */
 
 class User {
@@ -35,6 +32,7 @@ class User {
                                     last_name,
                                     phone`
       , [username, hashedPassword, first_name, last_name, phone])
+ 
     const user = result.row[0];
     return user;
   }
@@ -59,7 +57,7 @@ class User {
 
   static async updateLoginTimestamp(username) {
     const loginTime; //maybe DateTime?
-    await db.query (`UPDATE users SET last_login_at=$1 WHERE username=$2`, [loginTime, username])
+    await db.query(`UPDATE users SET last_login_at=$1 WHERE username=$2`, [loginTime, username])
   }
 
   /** All: basic info on all users:
@@ -93,21 +91,48 @@ class User {
                                   last_login_at
                                   FROM users
                                   WHERE 
-                                  username=$1`, 
-                                  [username])
+                                  username=$1`,
+      [username])
     const user = result.rows[0];
+
+    if ( user===undefined ){
+      throw new NotFoundError(`User Not Found`)
+    }
+
     return user;
   }
 
   /** Return messages from this user.
    *
-   * [{id, to_user, body, sent_at, read_at}]
+   * [{id, to_user (sent), body, sent_at, read_at}]
    *
    * where to_user is
    *   {username, first_name, last_name, phone}
    */
 
   static async messagesFrom(username) {
+
+    const mResult = await db.query(`SELECT m.id, m.to_username AS to_user, m.body, m.sent_at, m.read_at,
+                                      u.username, u.first_name, u.last_name, u.phone
+                                      FROM messages AS m
+                                      JOIN users AS u ON m.to_username = u.username
+                                      WHERE m.from_username=$1`, [username])
+                                      
+    const messages = mResult.rows.map(r => ({
+      id: r.id,
+      body: r.body,
+      sent_at: r.sent_at,
+      read_at: r.read_at,
+      to_user: {
+        username: r.username,
+        first_name: r.first_name,
+        last_name: r.last_name,
+        phone: r.phone
+      }
+    }));
+
+    return messages;
+
   }
 
   /** Return messages to this user.
@@ -119,8 +144,30 @@ class User {
    */
 
   static async messagesTo(username) {
+    const mResult = await db.query(`SELECT m.id, m.from_username AS from_user, m.body, m.sent_at, m.read_at,
+    u.username, u.first_name, u.last_name, u.phone
+    FROM messages AS m
+    JOIN users AS u ON m.from_username = u.username
+    WHERE m.to_username=$1`, [username])
+
+    const messages = mResult.rows.map(r => ({
+      id: r.id,
+      body: r.body,
+      sent_at: r.sent_at,
+      read_at: r.read_at,
+      from_user: {
+        username: r.username,
+        first_name: r.first_name,
+        last_name: r.last_name,
+        phone: r.phone
+      }
+    }));
+
+    return messages;
+
   }
 }
+
 
 
 module.exports = User;
