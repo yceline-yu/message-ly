0e68a75d6e72915c290275609402ada3c6ef3620
Sam Merham <sammerham-rithm@sams-mbp.lan>
2021-05-17 17:31:23 -0700

    added 1st route in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,19 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const User = require("../models/user")
+const { UnauthorizedError } = require("../expressError");
 
 /** POST /login: {username, password} => {token} */
-
+router.post('/login', async function (req, res, next){
+    const {username, password} = req.body;
+    if (User.authenticate(username, password)) {
+        let token = jwt.sign({username}, SECRET_KEY);
+        User.updateLoginTimestamp(username);
+        return res.json({token});
+    }
+    throw new UnauthorizedError('Invalid user/password');
+});
 
 /** POST /register: registers, logs in, and returns token.
  *
